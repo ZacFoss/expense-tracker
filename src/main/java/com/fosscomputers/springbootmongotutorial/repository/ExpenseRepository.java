9fa7b2138598ff9043cf4937e81d57e6e133994d
Zac Foss <zacfoss@gmail.com>
2023-02-04 13:58:59 -0500

    Initial commit

---
+++ src/main/java/com/fosscomputers/springbootmongotutorial/repository/ExpenseRepository.java
@@ -0,0 +1,12 @@
+package com.fosscomputers.springbootmongotutorial.repository;
+
+import com.fosscomputers.springbootmongotutorial.model.Expense;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface ExpenseRepository extends MongoRepository<Expense, String> {
+    @Query("{'name': ?0}")
+    Optional<Expense> findByName(String name);
+}
---
+++ src/test/java/com/fosscomputers/springbootmongotutorial/SpringBootMongoTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fosscomputers.springbootmongotutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
